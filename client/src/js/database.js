4455f75ced4b0d01074fdb7321e7b3bd6896848d
Steve Bour <bourpower@gmail.com>
2022-04-11 09:43:45 -0400

    added cache script, install, and put/get DB exports

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,25 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (id, value) => {
+  console.error('PUT request to JATEDB');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const objStor = tx.objectStore('jate');
+  const req = objStor.put({ id: id, value: value });
+  const res = await req;
+  console.log('Data saved to JATEDB', res)
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.error('Getting data from JATEDB');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const objStor = tx.objectStore('jate');
+  const req = objStor.getAll();
+  const res = await req;
+  console.log('Data saved to JATEDB', res)
+};
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -9,7 +9,6 @@ module.exports = () => {
     entry: {
       main: "./src/js/index.js",
       install: "./src/js/install.js",
-      // cards: "./src/js/cards.js",
     },
     output: {
       filename: "[name].bundle.js",
@@ -18,20 +17,19 @@ module.exports = () => {
     plugins: [
       new HtmlWebpackPlugin({
         template: "./index.html",
-        title: "Contact Cards",
+        title: "JATE",
       }),
 
       new InjectManifest({
         swSrc: "./src-sw.js",
         swDest: "src-sw.js",
       }),
-
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: "Contact Cards",
-        short_name: "Contact",
-        description: "Never forget your contacts!",
+        name: "Text Editor",
+        short_name: "JATE",
+        description: "Just Another Text Editor",
         background_color: "#225ca3",
         theme_color: "#225ca3",
         start_url: "/",
