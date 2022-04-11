d7e71bc4f7cf78ddf40134e741a220aebc346b83
Steve Bour <bourpower@gmail.com>
2022-04-11 09:51:01 -0400

    fixed service worker issue w/ stalewhilerevalidate

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,5 +1,5 @@
 const { offlineFallback, warmStrategyCache } = require("workbox-recipes");
-const { CacheFirst } = require("workbox-strategies");
+const { StaleWhileRevalidate, CacheFirst } = require("workbox-strategies");
 const { registerRoute } = require("workbox-routing");
 const { CacheableResponsePlugin } = require("workbox-cacheable-response");
 const { ExpirationPlugin } = require("workbox-expiration");
@@ -27,7 +27,6 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === "navigate", pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
 
 registerRoute(
   ({ request }) => ["style", "script", "worker"].includes(request.destination),
